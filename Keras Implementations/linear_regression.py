df27fe11f859f604bebfc0b7b86b0a28bce0ff2d
wanga26 <alex.wang@emc.com>
2018-10-26 20:06:00 +0800

    Save changes

---
+++ Keras Implementations/linear_regression.py
@@ -0,0 +1,73 @@
+from __future__ import absolute_import, division, print_function
+
+import tensorflow as tf
+import pandas as pd
+from tensorflow import keras
+from keras.models import Sequential
+from keras.layers import Input, Dense
+import matplotlib.pyplot as plt
+
+import numpy as np
+
+print(tf.__version__)
+
+
+# Downloading datasets
+boston_housing = keras.datasets.boston_housing
+
+(train_data, train_labels), (test_data, test_labels) = boston_housing.load_data()
+# Shuffle the training set
+order = np.argsort(np.random.random(train_labels.shape))
+train_data = train_data[order]
+train_labels = train_labels[order]
+
+mean = train_data.mean(axis=0)
+std = train_data.std(axis=0)
+train_data = (train_data - mean) / std
+test_data = (test_data - mean) / std
+
+
+column_names = ['CRIM', 'ZN', 'INDUS', 'CHAS', 'NOX', 'RM', 'AGE', 'DIS', 'RAD',
+                'TAX', 'PTRATIO', 'B', 'LSTAT']
+
+df = pd.DataFrame(train_data, columns=column_names)
+print (df.head())
+
+# Let's create the model
+model = Sequential()
+# model.add(Dense(1, activation='linear', input_shape=(train_data.shape[1],)))
+dense_layer = Dense(10, activation=None, input_shape=(train_data.shape[1],))
+model.add(dense_layer)
+model.add(Dense(10, activation=None, input_shape=(train_data.shape[1],)))
+model.add(Dense(10, activation=None, input_shape=(train_data.shape[1],)))
+model.add(Dense(1, activation=None, input_shape=(train_data.shape[1],)))
+
+optimizer = tf.train.RMSPropOptimizer(0.001)
+
+model.compile(loss='mse', optimizer=optimizer, metrics=['mae'])
+
+model.summary()
+
+def plot_history(history):
+  plt.figure()
+  plt.xlabel('Epoch')
+  plt.ylabel('Mean Abs Error [1000$]')
+  plt.plot(history.epoch, np.array(history.history['mean_absolute_error']),
+           label='Train Loss')
+  # plt.plot(history.epoch, np.array(history.history['val_mean_absolute_error']),
+  #          label = 'Val loss')
+  plt.legend()
+  plt.ylim([0, 5])
+
+
+history = model.fit(train_data, train_labels, epochs=100, batch_size=50, shuffle=False)
+
+# Perform on test data
+# [loss, mae]  = model.evaluate(test_data, test_labels, verbose=1)
+#
+# predict_labels = model.predict(test_data)
+#
+# plt.plot(test_labels)
+# plt.plot(predict_labels)
+
+
